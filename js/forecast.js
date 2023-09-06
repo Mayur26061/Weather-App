7b112bc2589165b794beb73de478ddb4bd1290a6
Mayur Rathod <mdridrdrr@gmail.com>
2023-09-07 00:06:57 +0530

    Added API data to UI

--- js/forecast.js
+++ js/forecast.js
@@ -3,15 +3,12 @@ let loc= undefined;
 let weather= undefined;
 const key = "PjAnxdEyfjUgDkJWpPAYmF1huEkH3VaC";
 // const input =
-const getWeather = async () => {
-    const location = await getCity("New York");
+const getWeather = async (city) => {
     const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-    const params = `${location.Key}?apikey=${key}`;
+    const params = `${city}?apikey=${key}`;
     const response = await fetch(base + params);
     const data = await response.json()
-    loc = location
-    weather = data[0]
-    console.log(data)
+    return data[0]
 };
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -20,11 +17,3 @@ const getCity = async (city) => {
   const data = await response.json();
   return data[0];
 };
-// getWeather('349727')
-// // getCity("mumbai")
-// //     .then((data) => {
-// //         console.log(data.Key)
-// //     })
-// //     .catch((err) => {
-// //         console.log(err);
-// //     })
--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 const form = document.querySelector(".change-location");
+const details = document.querySelector(".details");
+const card = document.querySelector(".card");
+const getUpdates = async (city) => {
+  const cityDet = await getCity(city);
+  const weather = await getWeather(cityDet.Key);
+  return { cityDet, weather };
+};
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
-  form.reset()
+  const city = form.city.value.trim();
+  form.reset();
+  getUpdates(city)
+    .then((data) => {
+      console.log(data);
+      details.innerHTML = `<h5 class="my-3">${data.cityDet.EnglishName}</h5>
+                <div class="my-3">${data.weather.WeatherText}</div>
+                  <div class="display-4 my-4">
+                    <span>${data.weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                  </div>
+                  `;
+      if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+
+      }
+    })
+    .catch((err) => console.log(err));
 });
