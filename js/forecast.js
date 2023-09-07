db9bc1c551eb50a7b397b9b50d4a2187a5cecc85
Mayur Rathod <mdridrdrr@gmail.com>
2023-09-07 23:33:51 +0530

    Loaded Image and icon for weather

--- js/forecast.js
+++ js/forecast.js
@@ -1,14 +1,14 @@
 console.log("Hello");
-let loc= undefined;
-let weather= undefined;
+let loc = undefined;
+let weather = undefined;
 const key = "PjAnxdEyfjUgDkJWpPAYmF1huEkH3VaC";
 // const input =
 const getWeather = async (city) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-    const params = `${city}?apikey=${key}`;
-    const response = await fetch(base + params);
-    const data = await response.json()
-    return data[0]
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const params = `${city}?apikey=${key}`;
+  const response = await fetch(base + params);
+  const data = await response.json()
+  return data[0]
 };
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const form = document.querySelector(".change-location");
 const details = document.querySelector(".details");
 const card = document.querySelector(".card");
+const icon = document.querySelector(".icon img")
+
+const updateUI = (data)=>{
+  const city = data.cityDet;
+  const weather = data.weather;
+  details.innerHTML = `<h5 class="my-3">${city.EnglishName}</h5>
+                          <div class="my-3">${weather.WeatherText}</div>
+                          <div class="display-4 my-4">
+                            <span>${weather.Temperature.Metric.Value}</span>
+                            <span>&deg;C</span>
+                          </div>`;
+  let src = "../img/night.svg";
+  if (weather.IsDayTime) {
+    src = "../img/day.svg";
+  }
+  let ic_src = `../img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute("src",ic_src)
+  card.firstElementChild.setAttribute("src", src);
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+}
 const getUpdates = async (city) => {
   const cityDet = await getCity(city);
   const weather = await getWeather(cityDet.Key);
@@ -11,19 +33,6 @@ form.addEventListener("submit", (ev) => {
   const city = form.city.value.trim();
   form.reset();
   getUpdates(city)
-    .then((data) => {
-      console.log(data);
-      details.innerHTML = `<h5 class="my-3">${data.cityDet.EnglishName}</h5>
-                <div class="my-3">${data.weather.WeatherText}</div>
-                  <div class="display-4 my-4">
-                    <span>${data.weather.Temperature.Metric.Value}</span>
-                    <span>&deg;C</span>
-                  </div>
-                  `;
-      if(card.classList.contains('d-none')){
-        card.classList.remove('d-none')
-
-      }
-    })
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
